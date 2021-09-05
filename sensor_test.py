dfcf907b96f49b22ec9f7974b555bab358a931a7
alexanderbasulto <62479628+alexanderbasulto@users.noreply.github.com>
2021-09-04 23:06:47 -0600

    Update sensor_test.py

--- sensor_test.py
+++ sensor_test.py
@@ -1,9 +1,13 @@
 import Adafruit_GPIO.I2C as I2C
 import board
-from adafruit_apds9960.apds9960 import APDS9960
+#from adafruit_apds9960.apds9960 import APDS9960
+import busio
+import adafruit_apds9960.apds9960
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_apds9960.apds9960.APDS9960(i2c)
 
 I2C.require_repeated_start()
-i2c = board.I2C()
+#i2c = board.I2C()
 sleep_time = 1
 apds = APDS9960(i2c)
 apds.enable_proximity = True
@@ -71,6 +75,6 @@ if __name__ == '__main__':
             print ( "*" * 40)
             time.sleep(sleep_time)
     except KeyboardInterrupt:
-        print ( "IoTHubClient stopped" )
+        print ( "Prueba Terminada" )
 
 # FIN DE LA FUNCION DEL SENSOR APDS9960
