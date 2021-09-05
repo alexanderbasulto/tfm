81a07522dd322d6cc1ab7747938d751230069fea
alexanderbasulto <62479628+alexanderbasulto@users.noreply.github.com>
2021-09-05 00:16:46 -0600

    Update test_apds9960.py

--- test_apds9960.py
+++ test_apds9960.py
@@ -1,7 +1,4 @@
-import Adafruit_GPIO.I2C as I2C
 import board
-import time
-from adafruit_apds9960.apds9960 import APDS9960
 import busio
 import adafruit_apds9960.apds9960
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -9,7 +6,6 @@ sensor = adafruit_apds9960.apds9960.APDS9960(i2c)
 
 I2C.require_repeated_start()
 
-sleep_time = 1
 apds = APDS9960(i2c)
 apds.enable_proximity = True
 apds.enable_gesture = True
